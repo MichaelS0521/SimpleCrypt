1a5f371171e57bafb5ee1660bbbd68007237f6cf
MichaelS0521 <michael@Zipcoders-MacBook-Pro-4.local>
2024-04-07 16:01:15 -0400

    Updated

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,8 +3,12 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    private Character cs;
+    private Character cf;
 
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
     }
 
     ROT13() {
@@ -12,8 +16,18 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        char[] chars = text.toCharArray();
 
-        return "";
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'a' && chars[i] <= 'm' || chars[i] >= 'A' && chars[i] <= 'M') {
+                int num = chars[i] + 13;
+                chars[i] = num;
+            } else if (chars[i] >= 'n' && chars[i] <= 'z' || chars[i] >= 'N' && chars[i] <= 'Z') {
+                int num = chars[i] - 13;
+                chars[i] = num;
+            }
+        }
+        return new String(chars);
     }
 
     public String encrypt(String text) {
---
+++ Crypto/src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String args[]) {
+        ROT13 test = new test() ROT13();
+        test.reader();
+    }
+}
